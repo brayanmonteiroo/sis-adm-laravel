6ba541a8f5aa758b3cdb09835375ecc78cfa859d
Brayan Monteiro <brayanmonteirooo@gmail.com>
2025-05-19 17:24:36 -0300

    criação da visualização e editar do perfil

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,7 +28,7 @@
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
                     data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#"><i class="fa-regular fa-address-card"></i> Perfil</a></li>
+                    <li><a class="dropdown-item" href="{{ route('profile.show') }}"><i class="fa-regular fa-address-card"></i> Perfil</a></li>
                     <li>
                         <hr class="dropdown-divider" />
                     </li>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -22,6 +23,13 @@ Route::group(['middleware' => 'auth'], function () {
 
     // Dashboard
     Route::get('/index-dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+    // Perfil
+     Route::get('/show-profile', [ProfileController::class, 'show'])->name('profile.show');
+     Route::get('/edit-profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/update-profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/edit-profile-password', [ProfileController::class, 'editPassword'])->name('profile.edit-password');
+    Route::put('/update-profile-password', [ProfileController::class, 'updatePassword'])->name('profile.update-password');
 
     // Usuários
     Route::get('/index-user', [UserController::class, 'index'])->name('user.index');
